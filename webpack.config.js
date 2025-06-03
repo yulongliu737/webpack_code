d8eedd4323a201f5fff0d8f852226f357f1bc3f3
yulongliu737 <15672248461@163.com>
2025-06-03 18:59:37 +0800

    webpack处理stylus文件

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,15 @@ module.exports = {
                     "css-loader", // 将css资源编译成commonjs的模块到js
                     "sass-loader", // 将sass编译成css文件
                 ]
+            },
+            {
+                test: /\.styl/,
+                use: [
+                    // 执行顺序，从下到上
+                    "style-loader", // 将js中css通过创建style标签添加到html文件中
+                    "css-loader", // 将css资源编译成commonjs的模块到js
+                    "stylus-loader", // 将stylus编译成css文件
+                ]
             }
         ]
     },
