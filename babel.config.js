2fdc3336a00e7f972f94fb0186f945645f3f9cd9
yulongliu737 <15672248461@163.com>
2025-06-04 18:38:16 +0800

    使用core-js+babel实现彻底解决浏览器兼容问题

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-    // 只能预设 能编译es6语法
-    presets: ["@babel/preset-env"]
-}
+    // 智能预设：能够编译ES6语法
+    presets: [
+        [
+            "@babel/preset-env",
+            {
+                useBuiltIns: "usage", // 按需加载自动引入
+                corejs: 3,
+            },
+        ],
+    ],
+};
--- src/main.js
+++ src/main.js
@@ -6,6 +6,16 @@ import "./sass/index.sass"
 import "./sass/index.scss"
 import "./stylus/index.styl"
 import "./css/iconfont.css"
+// import 'core-js' // 手动加载
+import 'core-js/es/promise' // 按需加载
 
 console.log(count(2,1))
 console.log(sum(1,2,3,4))
+
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(100)
+    })
+}).then(res => {
+    console.log(res)
+})
